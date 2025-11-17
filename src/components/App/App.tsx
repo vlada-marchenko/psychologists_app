19544a92815dc6f5d9f2750db07c77b4e1066ee7
vlada-marchenko <vvvvadya44@gmail.com>
2025-11-17 18:04:51 +0100

    check

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import { Header } from '../Header/Header.tsx'
-import Psychologists from '../../pages/Psychologists/Psychologists.tsx'
+// import Psychologists from '../../pages/Psychologists/Psychologists.tsx'
 import Home from '../../pages/Home/Home.tsx'
-import Favorites from '../../pages/Favorites/Favorites.tsx'
+// import Favorites from '../../pages/Favorites/Favorites.tsx'
 
 function App() {
-const isAuth = false
+// const isAuth = false
 
   return (
     <div>
       <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/psychologists" element={<Psychologists />}/>
-        <Route path="/favorites" element={isAuth && <Favorites /> }/>
+        {/* <Route path="/psychologists" element={<Psychologists />}/> */}
+        {/* <Route path="/favorites" element={isAuth && <Favorites /> }/> */}
       </Routes>
     </div>
   )
